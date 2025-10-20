205bd8713b903ba924e340619f3b828056723eec
Sayed-Touati <sayedtouati@outlook.com>
2025-10-21 00:35:45 +0100

    Setup Express server, Neon DB connection, and initialize transactions table

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 // const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
+import { sql } from './config/db.js';
 
 dotenv.config();
 
@@ -8,12 +9,30 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+
+        console.log("Database initialized successfully")
+    } catch (error) {
+        console.log("Error initializing DB", error)
+        process.exit(1) //status code 1 means failure, O is success
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-console.log("my port:", PORT);
-
-app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port', PORT);
+    });
 });
