8107023ed17cdca5f46e7c511d0aa26fba672c30
Sayed-Touati <sayedtouati@outlook.com>
2025-10-22 11:09:57 +0100

    Add POST and DELETE transactions routes with validation

--- Backend/server.js
+++ Backend/server.js
@@ -30,6 +30,25 @@ async function initDB() {
     }
 }
 
+app.get("/", (req, res) => {
+    res.send("it's working");
+});
+
+app.get("/api/transactions/:userId", async(req,res) => {
+    try {
+        const { userId } = req.params;
+        
+        const transactions = await sql`
+            SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+        `;
+
+        res.status(200).json(transactions);
+    } catch (error) {
+        console.log("Error creating transaction", error)
+        res.status(500).json({ message: "internal server error" });
+    }
+});
+
 app.post("/api/transactions", async (req,res) => {
     // title, amount, category, user_id
     try {
@@ -51,7 +70,30 @@ app.post("/api/transactions", async (req,res) => {
         console.log("Error creating transaction", error)
         res.status(500).json({ message: "internal server error" });
     }
-})
+});
+
+app.delete("/api/transactions/:id", async(req,res) => {
+    try {
+        const { id } = req.params;
+        
+        if(isNaN(parseInt(id))){
+            return res.status(400).json({ message: "Invalid Transaction ID" });
+        }
+        
+        const result = await sql`
+            DELETE FROM transactions WHERE id = ${id} RETURNING *;
+        `;
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Transaction not found" });
+        }
+
+        res.status(200).json({ message: "Transactions deleted successfully"});
+    } catch {
+        console.log("Error deleting the transaction", error)
+        res.status(500).json({ message: "internal server error" });
+    }
+});
 
 initDB().then(() => {
     app.listen(PORT, () => {
