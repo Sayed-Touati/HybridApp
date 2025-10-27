596c344f8da9294a082e13303fd6c96dd51a0a6b
Sayed-Touati <sayedtouati@outlook.com>
2025-10-27 22:09:29 +0100

    refactor: moved transactions routes to routes folder and added middleware

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import { sql } from './config/db.js';
 import rateLimiter from './Middleware/rateLimiter.js';
 
+import transactionsRoute from "./routes/transactionsRoute.js";
+
 dotenv.config();
 
 const app = express();
@@ -36,96 +38,7 @@ app.get("/", (req, res) => {
     res.send("it's working");
 });
 
-app.get("/api/transactions/:userId", async(req,res) => {
-    try {
-        const { userId } = req.params;
-        
-        const transactions = await sql`
-            SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
-        `;
-
-        res.status(200).json(transactions);
-    } catch (error) {
-        console.log("Error creating transaction", error)
-        res.status(500).json({ message: "internal server error" });
-    }
-});
-
-app.post("/api/transactions", async (req,res) => {
-    // title, amount, category, user_id
-    try {
-        const { title, amount, category, user_id } = req.body;
-
-        if (!title || !user_id || amount === undefined || !category) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        const transaction = await sql`
-            INSERT INTO  transactions(user_id,title,amount,category)
-            VALUES (${user_id},${title},${amount},${category})
-            RETURNING *;
-        `;
-
-        console.log(transaction);
-        res.status(201).json(transaction[0]);
-    } catch (error) {
-        console.log("Error creating transaction", error)
-        res.status(500).json({ message: "internal server error" });
-    }
-});
-
-app.delete("/api/transactions/:id", async(req,res) => {
-    try {
-        const { id } = req.params;
-        
-        if(isNaN(parseInt(id))){
-            return res.status(400).json({ message: "Invalid Transaction ID" });
-        }
-        
-        const result = await sql`
-            DELETE FROM transactions WHERE id = ${id} RETURNING *;
-        `;
-
-        if (result.length === 0) {
-            return res.status(404).json({ message: "Transaction not found" });
-        }
-
-        res.status(200).json({ message: "Transactions deleted successfully"});
-    } catch {
-        console.log("Error deleting the transaction", error)
-        res.status(500).json({ message: "internal server error" });
-    }
-});
-
-app.get("/api/transactions/summary/:userId", async (req, res) => {
-    try {
-        const { userId } = req.params;
-
-        const balanceResult = await sql`
-            SELECT COALESCE(SUM(amount), 0) as balance FROM transactions 
-            WHERE user_id = ${userId}
-        `;
-
-        const incomeResult = await sql`
-            SELECT COALESCE(SUM(amount), 0) as income FROM transactions
-            WHERE user_id = ${userId} AND amount > 0
-        `;
-
-        const expensesResult = await sql`
-            SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions
-            WHERE user_id = ${userId} AND amount < 0
-        `;
-
-        res.status(200).json({
-            balance: balanceResult[0].balance,
-            income: incomeResult[0].income,
-            expenses: expensesResult[0].expenses,
-        });
-    } catch (error) {
-        console.log("Error deleting the transaction", error)
-        res.status(500).json({ message: "internal server error" });
-    }
-});
+app.use("/api/transactions", transactionsRoute);
 
 initDB().then(() => {
     app.listen(PORT, () => {
